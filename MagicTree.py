1dd3c5fa0cbbc7e5b86bffda0910739802ab4e42
mukehrje <suman.mukherjee@live.com>
2017-07-09 00:29:27 +0300

    added pythong code

---
+++ MagicTree.py
@@ -0,0 +1,56 @@
+import sys
+import os
+from pprint import pprint as pp
+
+
+
+def first_user_input():
+	pp("Please enter how many times you like to loop: ")
+	number = raw_input()
+	if number <= 0:
+		raise ValueError("You entered invalid input, must be greater than zero") 
+	pp("You have entered {}".format(number))
+	try:
+		return int(number)
+	except:
+		ValueError("Cannot convert to number")
+		raise
+		
+		
+def second_user_input(numeric):
+	for i in range (numeric):
+		print "So user wishes to loop " + str(numeric) + " times"
+		print("Please enter the cycle times for {} loops".format(numeric))
+		allUserInputCycles = map(int,raw_input("enter positive numbers separated by a single space only: " ).split())	
+		for i in allUserInputCycles:
+			if i < 0:
+				raise ValueError("Negative cycle not excepted")
+		return allUserInputCycles
+	
+def calc_height(age):
+    if age==0:
+		return 1
+		print 1
+    elif age%2 == 0:
+		actualHeight = calc_height(age-1) + 1
+		return actualHeight
+    else:
+		actualHeight = 2*calc_height(age-1)
+		return actualHeight
+		
+def main():
+	firstUserInput = first_user_input()
+	secondUserInput = second_user_input(firstUserInput)
+	print "secondUserInput: " + str(secondUserInput)
+	for iLocalValue in secondUserInput:
+		finalHeight = calc_height(iLocalValue)
+		print "FinalHeight of magic tree: " + str(finalHeight) + " years"
+	
+if __name__ == '__main__':
+	main()
+
+	
+
+	
+	
+	
